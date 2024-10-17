0015a1f820c1fa6b16038df37ddc1fd3eb6240ef
abigail <abigail.chen@mail.utoronto.ca>
2024-10-17 00:45:21 -0400

    Task 2.2 not sure checkstyle

--- src/main/java/data_access/InMemoryUserDataAccessObject.java
+++ src/main/java/data_access/InMemoryUserDataAccessObject.java
@@ -42,7 +42,7 @@ public class InMemoryUserDataAccessObject implements SignupUserDataAccessInterfa
 
     @Override
     public String getCurrentUser() {
-        return "";
+        return this.currentUser;
     }
 
     @Override
